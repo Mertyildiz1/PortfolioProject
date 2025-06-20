854c039bc7a5a6c740cff454cbb8f12cc094c683
Mert <1yildizmert@gmail.com>
2025-06-20 16:05:12 +0300

    TC05 Added

--- src/test/java/tests/TC05.java
+++ src/test/java/tests/TC05.java
@@ -1,12 +1,12 @@
 package tests;
 
 import com.aventstack.extentreports.MediaEntityBuilder;
+import com.github.javafaker.Faker;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.Locates;
 import utilities.ConfigReader;
-import utilities.DataProviderClass;
 import utilities.Driver;
 import utilities.ReusableMethods;
 
@@ -16,48 +16,60 @@ import java.time.Duration;
 import static utilities.ReusableMethods.extentReports;
 import static utilities.ReusableMethods.extentTest;
 
-public class TC03 {
-
-    @Test(dataProvider = "invalidEmailData", dataProviderClass = DataProviderClass.class)
-    public void test03(String invalidEmail) throws IOException {
+public class TC05 {
+    @Test
+    public void test05() throws IOException {
 
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+        Faker faker = new Faker();
         SoftAssert softAssert = new SoftAssert();
-        ReusableMethods.report("Kayıt", "Kayıt Maili Geçersiz Karakterler", "Email adresinde kabul edilmeyen karakterler ile kayıt olmayı denerken hata mesajı alındığını doğrulama", "Mert Yıldız");
+        ReusableMethods.report("Kayıt", "Başarısız Kayıt", "Name alanı boş bırakıldığında kayıt işleminin başarısız olduğunu doğrulama", "Mert Yıldız");
         Locates locates = new Locates();
+
 
         try {
             // "https://www.automationexercise.com/login" adresine gidin.
             Driver.getDriver().get(ConfigReader.getProperty("loginPageUrl"));
+
+            String actualTitle = Driver.getDriver().getTitle();
+            String expectedTitle = "Automation Exercise - Signup / Login";
+            softAssert.assertEquals(actualTitle, expectedTitle);
+
             extentTest.info("Sayfaya başarılı bir şekilde gidildi");
 
             String initialTitle = Driver.getDriver().getTitle();
 
-            // Name alanına geçerli bir isim girin.
-            locates.SignUpPageNameArea.sendKeys(ConfigReader.getProperty("name"));
+            // Name alanını boş bırakın.
+            locates.SignUpPageNameArea.clear();
 
-            // "Email Address" alanına geçersiz formatta email adresi girin.
-            locates.SignUpPageEmailArea.sendKeys(invalidEmail);
-            extentTest.info("Geçersiz email adresi girildi: " + invalidEmail);
+            // "Email Address" alanına geçerli ve daha önce kullanılmamış bir email girin.
+            locates.SignUpPageEmailArea.sendKeys(ConfigReader.getProperty("unUsedMail"));
+            extentTest.info("Email Address alanına geçerli ve daha önce kullanılmamış mail adresi girildi : " + ConfigReader.getProperty("unUsedMail"));
 
             // "Sign Up" butonuna tıklayın.
             locates.SignUpPageSignUpButton.click();
+            extentTest.info("Sign up butonuna click yapıldı");
 
-            // Çıkan hata mesajını doğrulayın.
+            // Boş bırakılan "Name" alanı ile ilgili bir hata mesajı görüntülendiğini doğrulayın.
             ReusableMethods.bekle(1);
 
             String currentTitle = Driver.getDriver().getTitle();
             softAssert.assertEquals(currentTitle, initialTitle);
-            extentTest.info("Geçersiz mail için hata mesajı doğrulandı - Title değişmedi");
+            extentTest.info("Boş bırakılan Name alanı için hata mesajı doğrulandı - Title değişmedi");
 
             softAssert.assertAll();
             extentTest.pass("Test başarılı bir şekilde sonuçlandı");
+
         } catch (AssertionError | Exception e) {
+
             String screenshot = ReusableMethods.raporScreenShot();
             extentTest.fail("Test başarısız sonuçlandı!", MediaEntityBuilder.createScreenCaptureFromBase64String(screenshot).build());
             throw e;
+
         } finally {
+
             extentReports.flush();
+
         }
     }
 }
